d36e34292c9bd6aace748f6a26104c39a6dd78a7
Andrew Kigen <andrew.kigen@student.moringaschool.com>
2022-08-18 22:23:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(n){
   // Write your code here!
+let greeting 
+if( n<= 400 ) {
+  return "This one is on me!";
+} else if(n > 2500) {
+  return "No can do."
+} else if(n > 2000) {
+  return "I will gladly take your thirty bucks."
+}
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city==="NYC"?"Ok, sounds good.":"No go.");
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip) {
+    case "generous":
+      // code block
+      return 'Thank you so much.';
+      break;
+      case "not as generous":
+        // code block 
+        return "Thank you.";
+        break;
+        default:
+          // code block
+          return "Bye.";
+  }
+}  
